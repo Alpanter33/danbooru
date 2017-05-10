c93c48f93749098d78a140c0a04bb5c38d6c5eaf
Alpanter33 <githubv1@otoko.moe>
2017-05-10 13:04:14 +0200

    Update production.rb

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,14 +2,14 @@
 app_path = "/var/www/danbooru2/current"
 
 # Set unicorn options
-worker_processes 22
+worker_processes 5
 
 timeout 180
 #listen "127.0.0.1:9000", :tcp_nopush => true
 listen "/tmp/.unicorn.sock", :backlog => 512
 
 # Spawn unicorn master worker for user apps (group: apps)
-user 'albert', 'albert'
+user 'danbooru', 'danbooru'
 
 # Fill path to your app
 working_directory app_path
@@ -18,8 +18,8 @@ working_directory app_path
 rails_env = ENV['RAILS_ENV'] || 'production'
 
 # Log everything to one file
-stderr_path "/dev/null"
-stdout_path "/dev/null"
+stderr_path "#/log/unicorn.log"
+stdout_path "#/log/unicorn.log"
 
 # Set master PID location
 pid "#{app_path}/tmp/pids/unicorn.pid"
