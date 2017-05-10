ecdc3275a0197f2b119cb792534e7a8454cab663
Alpanter33 <githubv1@otoko.moe>
2017-05-10 18:14:34 +0200

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
 set :user, "danbooru"
 set :rails_env, "production"
 server "localhost", :roles => %w(web app db), :primary => true, :user => "danbooru"
-
-set :linked_files, fetch(:linked_files, []).push(".env.production")
